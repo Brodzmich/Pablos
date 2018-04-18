e7a025aa855c798619e889acf9ddce7ced7da813
Michal Brodzinski <brodzinsky.michal@gmail.com>
2018-04-18 18:52:05 +0200

    Allowed to repeat algorithms runs.

---
+++ Pablos/algorithmrunner.py
@@ -0,0 +1,10 @@
+from Pablos import matrixgenerator
+
+
+def run_algorithm(number_of_steps):
+    step_result = matrixgenerator.generate_initial_matrix()
+
+    for i in range(1, number_of_steps + 1):
+        step_result = matrixgenerator.perform_step(step_result)
+
+    print("{} {}".format(step_result.percent_similar, step_result.percent_unhappy))
--- Pablos/main.py
+++ Pablos/main.py
@@ -1,13 +1,7 @@
-from Pablos import matrixgenerator
+from Pablos import algorithmrunner
 
-# start of the program
-number_of_steps = 10
-step_result = matrixgenerator.generate_initial_matrix()
+# main file
 
-print("percent unhappy initial: ", step_result.percent_unhappy)
-print("percent similar initial: ", step_result.percent_similar)
-
-for i in range(1, number_of_steps + 1):
-    step_result = matrixgenerator.perform_step(step_result)
-    print("percent unhappy step {}: {} ".format(i, step_result.percent_unhappy))
-    print("percent similar step {}: {}".format(i, step_result.percent_similar))
+print("simmilar          unhappy")
+for i in range(100):
+    algorithmrunner.run_algorithm(10)
